756200d0281d8892ecfd48490abd2353456abcca
adm03 <globov.motya@mail.ru>
2024-12-25 13:55:57 +0700

    создание 5 лабы

---
+++ lab5/headers.h
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+#include <cstdlib>
+
+using namespace std;
+
+
+class Student {
+protected:
+	char* surname;
+	int age;
+	float average;
+public:
+	Student(int, float);
+	Student();
+	Student(const Student&);
+	~Student();
+
+	virtual void print();
+	void change_surname(char*);
+	void change_age(int);
+	void change_average(float);
+
+	void output(ostream&);
+
+	friend Student operator - (Student&, Student&);
+	Student operator + (Student&);
+	Student& operator = (const Student&);
+	Student& operator ++ ();
+	Student operator ++ (int);
+	operator int();
+
+	friend ostream& operator << (ostream&, const Student&);
+	friend istream& operator >> (istream&, Student&);
+};
+
+class Ex_student : public Student {
+	int ex_grades;
+public:
+	Ex_student(int ex);
+	void print();
+};
+
+class Debtor : public Student {
+	int debts;
+public:
+	Debtor(int deb);
+	void print();
+};
---
+++ lab5/main.cpp
@@ -0,0 +1,27 @@
+﻿#include "headers.h"
+#include <fstream>
+
+void main() {
+
+	Student fourth(2024, 5);
+	fourth.change_surname((char*)"jora");
+
+	Ex_student a(4);
+	a.change_surname((char*)"albert");
+	a.print();
+
+	Ex_student b(a);
+	b.print();
+
+	fourth.print();
+	a.print();
+
+	Student* r;
+	r = &fourth;
+	/*r = new Debtor(1);
+	r->change_surname((char*)"oleg");*/
+	r->print();
+
+	r = &a;
+	r->print();
+}
